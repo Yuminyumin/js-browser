263289ae03400e816696d79689c28500327a72d0
Yuminyumin <102581107+Yuminyumin@users.noreply.github.com>
2022-05-06 22:13:10 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.querySelector("div.hello first-child h1");
 
-if (isNaN(age) || age < 0){
-    console.log("Please write a real positive number");
-} else if(age < 18) {
-    console.log("You are too young.");
-} else if(age >=18 && age <=50) {
-    console.log("You can drink");
-} else if(age > 50 && age <=80) {
-    console.log("You can`t drinyou should exercise");
-} else if(age == 100) {
-    console.log("wow you are wise");
-} else if(age > 80) {
-    console.log("You can do whatever you want");
-} 
+function handleTitleClick(){
+    title.getElementsByClassName.color = "blue";
+}
+
+title.addEventListener("Click", handleTitleClick);
