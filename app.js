994a9fe295c9ac1bd69fed28496c0e99ca56bccd
Yuminyumin <102581107+Yuminyumin@users.noreply.github.com>
2022-05-06 22:26:56 +0900

    dd
    d

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const title = document.querySelector("div.hello first-child h1");
 
-function handleTitleClick(){
-    title.getElementsByClassName.color = "blue";
-}
+console.dir(title);
 
-title.addEventListener("Click", handleTitleClick);
+function handleTitleClick() {
+    title.style.color = "blue";
+    }
+function handleMouseEnter() {
+    title.innerText = "Mouse is here!";
+    }
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone!";
+    }
+
+title.addEventListener("click", handleTitleClick);  
+title.addEventListener("mouseenter", handleMouseEnter);  
+title.addEventListener("mouseleave", handleMouseLeave);
