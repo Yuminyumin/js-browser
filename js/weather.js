6785b2fa57a12b6774ea879715e76ff5ce60b022
Yuminyumin <102581107+Yuminyumin@users.noreply.github.com>
2022-05-08 19:38:24 +0900

    wheather

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,18 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "02a96cd6a897c27f8deeb6c286b5223e";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
-  }
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
   function onGeoError() {
     alert("Can't find you. No weather for you.");
   }
